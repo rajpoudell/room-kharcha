36de30a8b55eddcfb5e098264176c34b79669080
Raj Poudel <raj.poudeel@gmail.com>
2023-06-22 17:42:15 +0545

    complete html css version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./index.css"
+import { Center_firstline } from './Center_firstline';
+
 
 const App = () => {
   return (
     <div className='main'>
-        <div className="main--innerblock">
-            
+    <div className="main--innerblock">
+        <div className="main--center">
+            <Center_firstline />
+            <div className="main--appendchild">
+                <p>1.</p>
+                <p>tomato</p>
+                <p>date</p>
+                <p>X</p>
+            </div>
         </div>
-
-
     </div>
+
+
+</div>
   )
 }
 
