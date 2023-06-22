da635e106cf6429b1b6b94a54caabd5df93f59bd
Raj Poudel <raj.poudeel@gmail.com>
2023-06-22 16:59:15 +0545

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./index.css"
+
+const App = () => {
+  return (
+    <div className='main'>
+        <div className="main--innerblock">
+            
+        </div>
+
+
+    </div>
+  )
+}
+
+export default App;
